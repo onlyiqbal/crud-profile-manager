4268fe3f9db6a35895de3e90e280de95067e959f
iqbal <iqbalmenggala29@gmail.com>
2022-09-08 08:16:45 +0700

    develop file app blade

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('nama');
+            $table->date("tanggal_lahir");
+            $table->string("pekerjaan")->nullable();
+            $table->string("kota")->nullable();
+            $table->text("bio_profil")->nullable();
+            $table->string("gambar_profil")->default("default_profil.jpg");
+            $table->tinyInteger("background_profil")->default(1);
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Profile Manager</title>
+    <link rel="icon" href="{{ asset('img/favicon.png') }}" type="image/png">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -20,6 +21,75 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    {{-- NAVBAR --}}
+    <nav id="main-navbar" class="navbar navbar-expand-md navbar-dark bg-dark py-0">
+        <div class="container">
+            <a class="navbar-brand" href="{{ route('home') }}">
+                <span class="d-none" >ILKOOM</span>
+                <img src="{{ asset('img/ilkoom_logo.png') }}"
+                class="main-logo d-none d-md-inline" alt="ilkoom logo">
+                <img src="{{ asset('img/ilkoom_logo.png') }}"
+                class="small-logo d-inline d-md-none" alt="ilkoom logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                {{-- LEFT SIDE OF NAVBAR --}}
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link p-4 active" href="{{ route('home') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link p-4" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link p-4" href="#">Article</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link p-4" href="#">Gallery</a>
+                    </li>
+                </ul>
+
+                {{-- RIGHT SIDE OF NAVBAR --}}
+                <ul class="navbar-nav ms-auto">
+                    {{-- Authentication Links --}}
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link p-4" href="{{ route('login') }}">Login</a>
+                            </li>
+                        @endif
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link p-4" href="{{ route('register') }}">
+                                Register</a>
+                            </li>
+                        @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle p-4"
+                                href="#" data-bs-toggle="dropdown">
+                                {{ Auth::user()->nama }}
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-end">
+                                    <a class="dropdown-item p-4 py-md-2"
+                                    href="{{ route('logout') }}" onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}"
+                                    method="POST" class="d-none">
+                                    @csrf
+                                    </form>
+                                </div>
+                            </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -79,5 +149,77 @@
             @yield('content')
         </main>
     </div>
+
+    {{-- FOOTER --}}
+        <footer id="main-footer" class="text-white bg-dark py-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 text-center text-md-start">
+                        <a href="index.html">
+                        <img src="{{ asset('img/ilkoom_logo.png') }}" style="height: 60px;">
+                        </a>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Aperiam cumque, esse modi maxime veniam nulla delectus dolorem
+                        </p>
+                    </div>
+
+                    <div class="col-md-3 text-center">
+                        <h5>Community</h5>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-white">Activity</a></li>
+                            <li><a href="#" class="text-white">Members</a></li>
+                            <li><a href="#" class="text-white">Groups</a></li>
+                            <li><a href="#" class="text-white">Forums</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-3 text-center">
+                        <h5>Our Services</h5>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-white">Our mission</a></li>
+                            <li><a href="#" class="text-white">Help/Contact Us</a></li>
+                            <li><a href="#" class="text-white">Privacy Policy</a></li>
+                            <li><a href="#" class="text-white">Cookie Policy</a></li>
+                            <li><a href="#" class="text-white">Terms & Conditions</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-3 text-center text-md-start">
+                        <h5>Hubungi Kami</h5>
+                        <div class="text-nowrap"><i class="fas fa-envelope fa-fw me-3"></i>
+                        iqbalmenggala29@gmail.com</div>
+                        <div class="text-nowrap"><i class="fas fa-phone fa-fw me-3"></i>
+                        (021) 123456</div>
+                        <div class="text-nowrap"><i class="fas fa-globe fa-fw me-3"></i>
+                        https://onlyiqbal.github.io/</div>
+                    </div>
+                </div>
+
+                <div class="row mt-3 mt-md-0">
+                    <div class="col-md-3 me-md-auto text-center text-md-start">
+                        <small>&copy; ILKOOM {{ date("Y") }}</small>
+                    </div>
+                    <div class="col-md-3 text-center text-md-start">
+                        <div>
+                            <a href="#" class="text-white text-decoration-none me-2">
+                                <i class="fab fa-facebook fa-lg"></i>
+                            </a>
+                            <a href="#" class="text-white text-decoration-none me-2">
+                                <i class="fab fa-twitter fa-lg"></i>
+                            </a>
+                            <a href="#" class="text-white text-decoration-none me-2">
+                                <i class="fab fa-instagram fa-lg"></i>
+                            </a>
+                            <a href="#" class="text-white text-decoration-none me-2">
+                                <i class="fab fa-whatsapp fa-lg"></i>
+                            </a>
+                            <a href="#" class="text-white text-decoration-none me-2">
+                                <i class="fab fa-github fa-lg"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
 </body>
 </html>
